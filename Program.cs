a9d9ad8e772819eeaacad71e672cdaa83d074320
Matheus Emanuel <matheus.emanuelsc@gmail.com>
2024-11-15 15:24:04 -0300

    pacotes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.MapGet("/", () => "Ola mundo");
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456") {
+        return Results.Ok("Login com sucesso");
+    }
+    else { 
+       return  Results.Unauthorized();
+    }
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -20,4 +30,10 @@ app.UseHttpsRedirection();
 
 app.Run();
 
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set;} = default!;
+}
 
